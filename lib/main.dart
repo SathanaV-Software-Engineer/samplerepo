1de03e22e4744434779363a94bdb1515195a6076
= <=>
2024-02-09 15:33:30 +0530

    sample

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo today',
+      title: 'Flutter Demo today hai',
       theme: ThemeData(
         // This is the theme of your application.
         //
